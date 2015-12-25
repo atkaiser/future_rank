a5fb63378317341bf5946cfd9595dfe6f6be9094
Alex Kaiser <alextkaiser@gmail.com>
2015-12-25 00:02:20 -0500

    Added points script

---
+++ scripts/points.py
@@ -0,0 +1,16 @@
+"""
+Breakdown of how much each round at each type of tournament is worth
+"""
+
+
+def points(tournament_level):
+    if tournament_level == "Grand Slam":
+        return [10, 45, 90, 180, 360, 720, 1200, 2000]
+    elif tournament_level == "Masters 128":
+        return [10, 25, 45, 90, 180, 360, 600, 1000]
+    elif tournament_level == "Masters 96":
+        return [10, 45, 90, 180, 360, 600, 1000]
+    elif tournament_level == "ATP 500":
+        return [20, 45, 90, 180, 300, 500]
+    elif tournament_level == "ATP 250":
+        return [5, 20, 45, 90, 150, 250]
