958f3b61524622ea342dc1158ff2d72c84186800
Alex Kaiser <alextkaiser@gmail.com>
2017-02-21 13:19:12 -0800

    Making update script work in crontab

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,4 +10,7 @@ python generate_data.py > ../static/js/data.js
 
 cd ..
 
-/home/akaiser/.nvm/versions/node/v6.9.4/bin/gulp js
+export NVM_DIR="/home/akaiser/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+gulp js
