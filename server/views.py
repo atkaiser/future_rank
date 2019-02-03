4c7d76c86441c993668eb83d9528fb2e1a994b18
Alex Kaiser <alextkaiser@gmail.com>
2019-02-03 14:05:38 -0800

    Adding ability to get traffic times for personal use

--- server/views.py
+++ server/views.py
@@ -1,7 +1,18 @@
 from flask import render_template
 from server import app
+import requests
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/traffic/to-work')
+def toWork():
+    r = requests.get('http://sc2ls.mooo.com:10000/time?origin=5024+Ray+Ave,+Castro+Valley,+CA+94546&destination=777+Mariners+Island+Blvd,+San+Mateo,+CA+94404')
+    return r.text
+
+@app.route('/traffic/to-home')
+def toHome():
+    r = requests.get('http://sc2ls.mooo.com:10000/time?origin=777+Mariners+Island+Blvd,+San+Mateo,+CA+94404&destination=5024+Ray+Ave,+Castro+Valley,+CA+94546')
+    return r.text
