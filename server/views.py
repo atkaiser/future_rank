fa75c52c1c4ad52cde1f2fd693ef493cf10f9c1b
Alex Kaiser <alextkaiser@gmail.com>
2019-02-10 12:03:34 -0800

    Testing

--- server/views.py
+++ server/views.py
@@ -9,10 +9,12 @@ def index():
 
 @app.route('/traffic/to-work')
 def toWork():
-    r = requests.get('http://sc2ls.mooo.com:10000/time?origin=5024+Ray+Ave,+Castro+Valley,+CA+94546&destination=777+Mariners+Island+Blvd,+San+Mateo,+CA+94404')
-    return r.text
+    return "1"
+#    r = requests.get('http://sc2ls.mooo.com:10000/time?origin=5024+Ray+Ave,+Castro+Valley,+CA+94546&destination=777+Mariners+Island+Blvd,+San+Mateo,+CA+94404')
+#    return r.text
 
 @app.route('/traffic/to-home')
 def toHome():
-    r = requests.get('http://sc2ls.mooo.com:10000/time?origin=777+Mariners+Island+Blvd,+San+Mateo,+CA+94404&destination=5024+Ray+Ave,+Castro+Valley,+CA+94546')
-    return r.text
+    return "2"
+#    r = requests.get('http://sc2ls.mooo.com:10000/time?origin=777+Mariners+Island+Blvd,+San+Mateo,+CA+94404&destination=5024+Ray+Ave,+Castro+Valley,+CA+94546')
+#    return r.text
